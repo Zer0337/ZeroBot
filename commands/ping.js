a897d04ba1b99a529928faaf347b6e2e15291cde
Zer0337 <52370778+Zer0337@users.noreply.github.com>
2019-06-30 21:46:55 +1200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,4 @@
 exports.run = (client, message, args) => {
-    message.channel.send("pong!").catch(console.error);
+    const m = await message.channel.send("Pinging...");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }
