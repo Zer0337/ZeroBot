dd8df673eea83b6341c7e149543d97fc1349c2e7
Zer0337 <52370778+Zer0337@users.noreply.github.com>
2019-06-30 22:38:34 +1200

    Update urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -2,6 +2,7 @@
 const ud = require('urban-dictionary')
 
 exports.run = (client, message, args) => {
+
   var definition = args.toString();
   message.channel.startTyping();
   ud.term(definition, (error, entries, tags, sounds) => {
@@ -9,7 +10,16 @@ exports.run = (client, message, args) => {
     console.error("Error: " + error.message)
     message.channel.send("That word is undefined.")
   } else {
-    message.channel.send(entries[0].definition)
+    const embed = {
+  "title": "Urban definition for: " + args.toString,
+  "description": entries[0].definition,
+  "color": 16711684,
+  "footer": {
+    "text": "ZeroBot, Powering Zero's Den"
+  }
+};
+channel.send({ embed });
+    // message.channel.send(entries[0].definition)
   }
 })
 message.channel.stopTyping();
