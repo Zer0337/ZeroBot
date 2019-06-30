e8e205d80fb6867ab5355d325282d44117a86449
Zer0337 <52370778+Zer0337@users.noreply.github.com>
2019-06-30 22:26:28 +1200

    Update urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -3,12 +3,13 @@ const ud = require('urban-dictionary')
 
 exports.run = (client, message, args) => {
   var definition = args.toString();
-
+  channel.startTyping();
   ud.term(definition, (error, entries, tags, sounds) => {
   if (error) {
     console.error(error.message)
   } else {
     message.channel.send(entries[0].definition)
+    channel.stopTyping();
   }
 })
 
