e96abd6f52903ce09902eb32873df79879fe4288
Zer0337 <52370778+Zer0337@users.noreply.github.com>
2019-06-30 22:05:08 +1200

    Update urban.js

--- commands/urban.js
+++ commands/urban.js
@@ -1,16 +1,12 @@
 var urban = require("urban");
 
 exports.run = (client, message, args) => {
-    var targetWord = args == "" ? urban.random() : urban(args);
-					targetWord.first(function(json) {
-							if (json) {
-								var message = "Urban Dictionary: **" + json.word + "**\n\n" + json.definition;
-								if (json.example) {
-										message = message + "\n\n__Example__:\n" + json.example;
-								}
-						    message.channel.send( essage);
-							} else {
-								message.channel.send("No matches found");
-							}
-});
+    if(args == "") {
+	    var target = urban.random();
+      message.channel.send(target);
+    }
+    else {
+      var target = urban(args)
+      message.channel.send(target);
+    }
 }
