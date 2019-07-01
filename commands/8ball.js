93d0875f4a83e48f81ed13f07df5b5b8d4bbb926
Zer0337 <52370778+Zer0337@users.noreply.github.com>
2019-07-01 19:22:35 +1200

    Create 8ball.js

---
+++ commands/8ball.js
@@ -0,0 +1,15 @@
+exports.run = async (client, message, args) => {
+
+const eightballAnswers = ["Very doubtful.", "Concentrate and ask again.", "Signs point to yes.", "You may rely on it."];
+
+const eightBallAnswersIndex = Math.floor(Math.random() * eightballAnswers.length);
+
+const embed = {
+  "description": eightBallAnswersIndex,
+  "footer": {
+    "text": "ZeroBot, Powering Zero's Den"
+  }
+};
+channel.send({ embed });
+
+}
