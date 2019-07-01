6ad9af7a1cb60e834960a884384d1dbd9ceb567b
Zer0337 <52370778+Zer0337@users.noreply.github.com>
2019-07-01 19:16:32 +1200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -30,22 +30,4 @@ if (args == "urban") {
   message.channel.send("**Usage is:**\n.urban <word/phrase>\n**Examples:**\n.urban yeet\n:warning: The API response time for Urban Dictionary is slow.")
 }
 
-if (args) {
-client.helpcmds = new Enmap();
-
-fs.readdir("./helpcmds/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./helpcmds/${file}`);
-    let helpcmdname = file.split(".")[0];
-    console.log(`Attempting to load helpcmds ${helpcmdname}`);
-    client.helpcmds.set(helpcmdname, props);
-  });
-});
-
-const helpcmd = client.helpcmds.get(args);
-if (!helpcmd) return;
-}
-
 }
