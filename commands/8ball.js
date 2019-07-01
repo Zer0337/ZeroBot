0f44853c7e065d579601e68b1343da1f28f98e51
Zer0337 <52370778+Zer0337@users.noreply.github.com>
2019-07-01 19:38:13 +1200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,20 +1,9 @@
 exports.run = async (client, message, args) => {
 
-const eightballAnswers = ["Very doubtful.", "Concentrate and ask again.", "Signs point to yes.", "You may rely on it."];
+const eightballAnswers = ["Very doubtful.", "Concentrate and ask again.", "Signs point to yes.", "You may rely on it.", "Yes - definitely.", "Yes.", "Cannot predict now.", "Outlook not so good."];
 
 const eightBallAnswersIndex = Math.floor(Math.random() * eightballAnswers.length);
 
-if(args = "") {
-  const embed = {
-    "description": "Are you asking me a question? -_-",
-    "footer": {
-      "text": "ZeroBot, Powering Zero's Den"
-    }
-  };
-message.channel.send({ embed });
-}
-
-if(args > 1) {
 const embed = {
   "description": eightballAnswers[eightBallAnswersIndex],
   "footer": {
@@ -22,6 +11,5 @@ const embed = {
   }
 };
 message.channel.send({ embed });
-}
 
 }
